16271631165a55efdb92d0953cac770bb6f816bc
RemilioNubilio <275382225+RemilioNubilio@users.noreply.github.com>
2026-08-10 15:14:02 -0700

    Remove dead getBalance ownership gate (superseded by heldSkillMints)

--- packages/core/src/notes/index.ts
+++ packages/core/src/notes/index.ts
@@ -1,5 +1,4 @@
 export { postNote, readNotes, deleteNote, postAgentNote, readAgentNotes } from "./notes.js";
 export type { PostNoteInput, ReadNotesOptions, PostAgentNoteInput } from "./notes.js";
-export { getBalance } from "./balance.js";
 export { heldSkillMints, invalidateHeldMints } from "./holdings.js";
 export { getSolBalance, canAffordSkill, TX_FEE_BUFFER_LAMPORTS } from "./solBalance.js";
